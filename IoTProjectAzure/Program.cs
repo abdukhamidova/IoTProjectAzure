dc8b174b92c99de901e73697da1f380f03066965
abdukhamidova <ul0278773@edu.uni.lodz.pl>
2025-01-03 17:49:25 +0100

    Setting files

--- IoTProjectAzure/Program.cs
+++ IoTProjectAzure/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//string deviceConnectionString = "HostName=LittleHubIot.azure-devices.net;DeviceId=DeviceDemoSdk1;SharedAccessKey=Kh1uVOuO+CjHbvXCW3wPIbQnZuK3A1mXd1Dsp5SNuTk="; 
