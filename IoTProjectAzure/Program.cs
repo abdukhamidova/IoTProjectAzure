0fb50f12b85baaa2e7125a9e17cf4ba1ae7b58bd
abdukhamidova <ul0278773@edu.uni.lodz.pl>
2025-01-07 12:12:57 +0100

    setting files 3

--- IoTProjectAzure/Program.cs
+++ IoTProjectAzure/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//string deviceConnectionString = "HostName=LittleHubIot.azure-devices.net;DeviceId=DeviceDemoSdk1;SharedAccessKey=Kh1uVOuO+CjHbvXCW3wPIbQnZuK3A1mXd1Dsp5SNuTk="; 
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+using (var client = new OpcClient("opc.tcp://localhost:4840/"))
+{
+    client.Connect();
+    OpcReadNode[] commands = new OpcReadNode[] {
+    new OpcReadNode("ns=2;s=Device 1/ProductionStatus", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/ProductionStatus"),
+    new OpcReadNode("ns=2;s=Device 1/ProductionRate", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/ProductionRate"),
+    new OpcReadNode("ns=2;s=Device 1/WorkorderId", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/WorkorderId"),
+    new OpcReadNode("ns=2;s=Device 1/Temperature", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/Temperature"),
+    new OpcReadNode("ns=2;s=Device 1/GoodCount", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/GoodCount"),
+    new OpcReadNode("ns=2;s=Device 1/BadCount", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/BadCount"),
+    new OpcReadNode("ns=2;s=Device 1/DeviceError", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/DeviceError"),
+};
+
+    IEnumerable<OpcValue> job = client.ReadNodes(commands);
+
+    foreach (var item in job)
+    {
+        Console.WriteLine(item.Value);
+    }
+}
